0095a24163fb13e846fcc0445c457bed617fceca
Gourav Ghuge <you@example.com>
2024-07-30 10:01:25 +0530

    start the reposirory

---
+++ Priority_Queue/demo.java
@@ -0,0 +1,20 @@
+package Priority_Queue;
+
+import java.util.PriorityQueue;
+
+public class demo {
+
+    public static void main(String[] args) {
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        pq.add(23);
+        pq.add(15);
+        pq.add(48);
+
+
+
+        System.out.println(pq.peek());
+
+    }
+}
